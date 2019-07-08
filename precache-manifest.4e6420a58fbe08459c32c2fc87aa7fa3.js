d7d6b6b73544dc01bcb1ac47cfcf215d44c1dff2
Tabbs18 <tabithamclaughlin500@yahoo.com>
2019-07-08 16:26:12 -0400

    Updates

---
+++ precache-manifest.4e6420a58fbe08459c32c2fc87aa7fa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f848d92cabf715356fdde6f2358fc149",
+    "url": "/Tabbs18/ClickyGame.git/index.html"
+  },
+  {
+    "revision": "12b588e9953d7cf86603",
+    "url": "/Tabbs18/ClickyGame.git/static/css/main.0e1f2d65.chunk.css"
+  },
+  {
+    "revision": "c0d591e7ad99391a283a",
+    "url": "/Tabbs18/ClickyGame.git/static/js/2.0e040e98.chunk.js"
+  },
+  {
+    "revision": "12b588e9953d7cf86603",
+    "url": "/Tabbs18/ClickyGame.git/static/js/main.70a9ec5e.chunk.js"
+  },
+  {
+    "revision": "f1a5b926fe7847901561",
+    "url": "/Tabbs18/ClickyGame.git/static/js/runtime~main.60e68274.js"
+  }
+]);
